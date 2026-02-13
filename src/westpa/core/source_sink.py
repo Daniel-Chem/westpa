5fefc26efad732bbd332207aad50a630cd547d11
Jeff Thompson <jeffpthompson@proton.me>
2026-02-12 22:49:35 -0700

    Rename 'random_sample' -> 'random_choice'.

--- src/westpa/core/source_sink.py
+++ src/westpa/core/source_sink.py
@@ -67,9 +67,9 @@ class Source:
         args = f'{self.states}, p={self.p.tolist()}'
         return type(self).__name__ + '(' + args + ')'
 
-    def random_sample(self, k, seed=None):
+    def random_choice(self, size=None, seed=None):
         rng = np.random.default_rng(seed)
-        return rng.choice(self.states, p=self.p, size=k).tolist()
+        return rng.choice(self.states, p=self.p, size=size).tolist()
 
 
 class Sink(Container):
