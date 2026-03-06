b5e9fe9fff6900ad69f586bc8599ff06ca36766c
Jeff Thompson <jeffpthompson@proton.me>
2026-03-05 23:32:34 -0700

    Revise State API.

--- src/westpa/core/state.py
+++ src/westpa/core/state.py
@@ -2,68 +2,54 @@ import numpy as np
 
 
 class State:
-    """Describes a specific configuration (i.e., a microstate) of the system being simulated.
+    """Describes a specific configuration (i.e., a microstate) of the model being simulated.
 
-    States may be created by providing a coordinate tuple, a reference to a
-    file containing coordinate data, or a unique identifier. At least one
-    parameter must be provided.
+    The propagator determines the required shape for `coord` or format for `file`.
 
     Parameters
     ----------
-    coord : 1-D array-like, optional
-        Coordinate tuple.
+    coord : 1-D array_like, optional
+        Coordinates of the state.
     file : str, optional
-        Reference (e.g., an absolute path or URI) to a file containing coordinate data.
-    id : str or int, optional
-        Unique identifier.
+        Identifier (e.g., an absolute path or URI) of a file containing
+        coordinate data for the state.
+    label : str, optional
+        Descriptive label for the state.
 
     Attributes
     ----------
-    coord : numpy.ndarray or None
-    file : str or None
-    id : str, int, or None
+    coord : numpy.ndarray
+    file : str
+    label : str
 
     Examples
     --------
-
-    2-D system:
 
     >>> import westpa
     >>> westpa.State(coord=[0.0, 0.0])
     State(coord=[0.0, 0.0])
 
-    Coordinates stored in a local file:
+    If coordinates are stored on disk, use the `file` parameter:
 
     >>> westpa.State(file='/path/to/file.xyz')
     State(file='/path/to/file.xyz')
 
-    Discrete system with integer state IDs:
-
-    >>> westpa.State(id=0)
-    State(id=0)
-
     """
 
-    def __init__(self, *, coord=None, file=None, id=None):
-        if coord is None and file is None and id is None:
-            raise ValueError("'coord', 'file', or 'id' must be provided")
+    def __init__(self, *, coord=None, file=None, label=None):
+        if coord is None and file is None:
+            raise ValueError("either 'coord' or 'file' must be provided")
 
         if coord is not None:
             coord = np.asarray(coord)
-            if not coord.ndim == 1:
+            if not np.issubdtype(coord.dtype, np.number):
+                raise TypeError("scalar type of 'coord' must be numeric")
+            if coord.ndim != 1:
                 raise ValueError("'coord' must be a 1-D array")
-
-        if id is not None and not isinstance(id, (str, int)):
-            raise TypeError("'id' must be a string or integer")
 
         self._coord = coord
         self._file = str(file) if file is not None else None
-        self._id = id
-
-    @property
-    def id(self):
-        """State ID."""
-        return self._id
+        self._label = label or ''
 
     @property
     def coord(self):
@@ -75,19 +61,18 @@ class State:
         """File containing coordinate data."""
         return self._file
 
+    @property
+    def label(self):
+        """State label."""
+        return self._label
+
     def __repr__(self):
-        args = ', '.join(f'{k}={v!r}' for k, v in self.to_dict().items())
-        return type(self).__name__ + '(' + args + ')'
-
-    def to_dict(self):
-        d = {}
-        for name in ('coord', 'file', 'id'):
-            if (value := getattr(self, name)) is not None:
-                if name == 'coord':
-                    value = value.tolist()
-                d[name] = value
-        return d
-
-    @classmethod
-    def from_dict(cls, d):
-        return cls(**d)
+        kwargs = {}
+        if self._coord is not None:
+            kwargs['coord'] = self._coord.tolist()
+        if self._file is not None:
+            kwargs['file'] = self._file
+        if self._label:
+            kwargs['label'] = self._label
+        params = ', '.join(f'{k}={v!r}' for k, v in kwargs.items())
+        return type(self).__name__ + '(' + params + ')'
