e0bec7fbdc76de5574c0f5301b7d5d8be47641d4
Jeff Thompson <jeffpthompson@proton.me>
2026-03-09 23:56:01 -0600

    Remove State.label attribute.

--- src/westpa/core/state.py
+++ src/westpa/core/state.py
@@ -4,7 +4,7 @@ import numpy as np
 class State:
     """Describes a specific configuration (i.e., a microstate) of the model being simulated.
 
-    The propagator determines the required shape for `coord` or format for `file`.
+    Either the `coord` parameter or the `file` parameter must be specified.
 
     Parameters
     ----------
@@ -13,30 +13,33 @@ class State:
     file : str, optional
         Identifier (e.g., an absolute path or URI) of a file containing
         coordinate data for the state.
-    label : str, optional
-        Descriptive label for the state.
 
     Attributes
     ----------
     coord : numpy.ndarray
     file : str
-    label : str
 
     Examples
     --------
 
     >>> import westpa
-    >>> westpa.State(coord=[0.0, 0.0])
-    State(coord=[0.0, 0.0])
+    >>> import numpy as np
 
-    If coordinates are stored on disk, use the `file` parameter:
+    Coordinates stored in memory:
 
-    >>> westpa.State(file='/path/to/file.xyz')
+    >>> westpa.State(coord=[0., 0.])
+    State(coord=array([0., 0.]))
+    >>> westpa.State(coord=np.array([0., 0.], dtype=np.float32))
+    State(coord=array([0., 0.], dtype=float32))
+
+    Coordinates stored in an external file:
+
+    >>> westpa.State(file='/path/to/file.xyz'))
     State(file='/path/to/file.xyz')
 
     """
 
-    def __init__(self, *, coord=None, file=None, label=None):
+    def __init__(self, *, coord=None, file=None):
         if coord is None and file is None:
             raise ValueError("either 'coord' or 'file' must be provided")
 
@@ -49,7 +52,6 @@ class State:
 
         self._coord = coord
         self._file = str(file) if file is not None else None
-        self._label = label or ''
 
     @property
     def coord(self):
@@ -61,18 +63,11 @@ class State:
         """File containing coordinate data."""
         return self._file
 
-    @property
-    def label(self):
-        """State label."""
-        return self._label
-
     def __repr__(self):
         kwargs = {}
         if self._coord is not None:
-            kwargs['coord'] = self._coord.tolist()
+            kwargs['coord'] = self._coord
         if self._file is not None:
             kwargs['file'] = self._file
-        if self._label:
-            kwargs['label'] = self._label
         params = ', '.join(f'{k}={v!r}' for k, v in kwargs.items())
         return type(self).__name__ + '(' + params + ')'
